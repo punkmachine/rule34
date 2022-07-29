4fdeba11214ce70d5f20643746ad6dc562d314d7
punkmachine <alex_rassydihin@mail.ru>
2022-07-29 20:42:30 +0600

    Добавляет vuetify в проект.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-	<nav>
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link>
-	</nav>
-	<router-view />
+	<v-app>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
-<style lang="scss">
-</style>
+<script>
+export default {
+	name: "App",
+
+	data: () => ({
+		//
+	}),
+};
+</script>
